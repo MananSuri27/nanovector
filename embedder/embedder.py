4e3e32f34549e1818535c15f6f097c22d167cea5
manansuri27 <manansuri27@gmail.com>
2023-09-13 19:14:20 +0530

    embedder integrated

---
+++ embedder/embedder.py
@@ -0,0 +1,9 @@
+from sentence_transformers import SentenceTransformer
+
+class Embedder:
+    def __init__(self, model_name):
+        self.model_name = model_name
+        self.model = SentenceTransformer(model_name)
+
+    def generate_embeddings(self, sentences):
+        return self.model.encode(sentences)
